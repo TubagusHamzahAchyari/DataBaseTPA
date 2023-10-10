554bd3bebc513f16558cd8a5af92060b9e5ef8d3
TubagusHamzahAchyari <achyarimailbox@gmail.com>
2023-10-11 06:57:47 +0700

    Data Base TPA (2)

--- mysite/database_tpa/models.py
+++ mysite/database_tpa/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
+class Jadwal(models.Model):
+    hari_mengajar = models.CharField(max_length=30, blank=True, null=True)
+
+class Guru(models.Model):
+    nama_guru = models.CharField(max_length=30, blank=True, null=True)
+    jadwal_mengajar = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
+
 class Siswa(models.Model):
     nama_lengkap = models.CharField(max_length=50, blank=True,null=True)
     nama_panggilan = models.CharField(max_length=20, blank=True, null=True)
+    nama_orang_tua = models.CharField(max_length=50, blank=True,null=True)
+    kontak = models.CharField(max_length=30, blank=True,null=True)
+    alamat = models.TextField(blank=True, null=True)
+    guru = models.ForeignKey(Guru, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+
 
