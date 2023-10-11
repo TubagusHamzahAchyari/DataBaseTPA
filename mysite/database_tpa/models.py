73336deacb58da64be81422c4cdf40fb74481c5f
TubagusHamzahAchyari <achyarimailbox@gmail.com>
2023-10-11 07:55:59 +0700

    Data Base TPA (collect statics)

--- mysite/database_tpa/models.py
+++ mysite/database_tpa/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class SiswaPDB(models.Model):
+    nama_lengkap = models.CharField(max_length=50, blank=True, null=True)
+    nama_panggilan = models.CharField(max_length=20, blank=True, null=True)
+    nama_wali = models.CharField(max_length=50, blank=True, null=True)
+    kontak = models.CharField(max_length=30, blank=True, null=True)
+    alamat = models.TextField(blank=True, null=True)
+
 class Jadwal(models.Model):
     hari_mengajar = models.CharField(max_length=30, blank=True, null=True)
 
@@ -10,7 +17,7 @@ class Guru(models.Model):
 class Siswa(models.Model):
     nama_lengkap = models.CharField(max_length=50, blank=True,null=True)
     nama_panggilan = models.CharField(max_length=20, blank=True, null=True)
-    nama_orang_tua = models.CharField(max_length=50, blank=True,null=True)
+    nama_wali = models.CharField(max_length=50, blank=True,null=True)
     kontak = models.CharField(max_length=30, blank=True,null=True)
     alamat = models.TextField(blank=True, null=True)
     guru = models.ForeignKey(Guru, null=True, blank=True, on_delete=models.SET_NULL)
