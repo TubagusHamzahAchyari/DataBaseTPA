b48c46a912df37ebfadb1c336ab5be751a51d61a
TubagusHamzahAchyari <achyarimailbox@gmail.com>
2023-10-11 09:54:07 +0700

    Data Base TPA (collect statics)

--- mysite/database_tpa/models.py
+++ mysite/database_tpa/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+class Jadwal(models.Model):
+    jadwal = models.CharField(max_length=30, blank=True, null=True)
+
+class Guru(models.Model):
+    nama_guru = models.CharField(max_length=30, blank=True, null=True)
+    jadwal_mengajar = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
 
 class SiswaPDB(models.Model):
     nama_lengkap = models.CharField(max_length=50, blank=True, null=True)
@@ -6,13 +12,9 @@ class SiswaPDB(models.Model):
     nama_wali = models.CharField(max_length=50, blank=True, null=True)
     kontak = models.CharField(max_length=30, blank=True, null=True)
     alamat = models.TextField(blank=True, null=True)
+    jadwal_mengaji = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
 
-class Jadwal(models.Model):
-    hari_mengajar = models.CharField(max_length=30, blank=True, null=True)
 
-class Guru(models.Model):
-    nama_guru = models.CharField(max_length=30, blank=True, null=True)
-    jadwal_mengajar = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
 
 class Siswa(models.Model):
     nama_lengkap = models.CharField(max_length=50, blank=True,null=True)
@@ -21,6 +23,7 @@ class Siswa(models.Model):
     kontak = models.CharField(max_length=30, blank=True,null=True)
     alamat = models.TextField(blank=True, null=True)
     guru = models.ForeignKey(Guru, null=True, blank=True, on_delete=models.SET_NULL)
+    jadwal_mengaji = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
     siswa_pdb = models.ForeignKey(SiswaPDB, null=True, blank=True, on_delete=models.SET)
 
 
