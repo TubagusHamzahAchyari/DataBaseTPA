1083bb13c42a9d8fb69a932531922fe5b2e88d35
TubagusHamzahAchyari <achyarimailbox@gmail.com>
2023-10-18 20:49:24 +0700

    Data Base TPA (1.0)

--- mysite/database_tpa/admin.py
+++ mysite/database_tpa/admin.py
@@ -1,6 +1,7 @@
+
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.models import User
+import babel.numbers
 
 class SiswaInLineJadwal(admin.TabularInline):
     model = Siswa
@@ -39,9 +40,33 @@ class SiswaPDBAdmin(admin.ModelAdmin):
 
 
 class SiswaAdmin(admin.ModelAdmin):
-    list_display = ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak', 'alamat', 'guru', 'jadwal_mengaji', 'keuangan')
+    list_display = ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak',
+                    'alamat', 'guru', 'jadwal_mengaji', 'babel_spp','jenis_kelamin', 'keuangan','jilid_umi')
     raw_id_fields = ('siswa_pdb',)
-    search_fields = ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak', 'alamat',)
+    search_fields = ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak', 'alamat','keuangan__status_keuangan','jilid_umi','jenis_kelamin','jadwal_mengaji__jadwal','guru__nama_guru','spp')
+    actions = ['save_all_selected','keuangan_satu','keuangan_dua','keuangan_tiga']
+
+    def babel_spp(self, obj):
+        return babel.numbers.format_currency(obj.spp, 'IDR', locale='id_ID')
+    babel_spp.shortdescription = 'SPP'
+    def save_all_selected(modeladmin, request, queryset):
+        for obj in queryset:
+            obj.save()
+
+    save_all_selected.short_description = "Update Data"
+    def keuangan_satu(self, request, queryset):
+        queryset.update(keuangan=1)
+
+    keuangan_satu.short_description = "Belum ditagih"
+    def keuangan_dua(self, request, queryset):
+        queryset.update(keuangan=2)
+
+    keuangan_dua.short_description = "Sudah ditagih"
+
+    def keuangan_tiga(self, request, queryset):
+        queryset.update(keuangan=3)
+
+    keuangan_tiga.short_description = "Sudah bayar"
 
     def get_list_display(self, request):
         user_exists = request.user.username == 'gurutpa'
@@ -50,7 +75,8 @@ class SiswaAdmin(admin.ModelAdmin):
             self.list_display_links = None
             return ('nama_lengkap', 'nama_panggilan', 'guru', 'jadwal_mengaji')
         else:
-            return ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak', 'alamat', 'guru', 'jadwal_mengaji', 'keuangan')
+            return ('nama_lengkap', 'nama_panggilan', 'nama_wali', 'kontak',
+                    'alamat', 'guru', 'jadwal_mengaji', 'babel_spp','jenis_kelamin', 'keuangan','jilid_umi')
 
 
 
@@ -60,8 +86,8 @@ admin.site.register(Guru, GuruAdmin)
 admin.site.register(SiswaPDB, SiswaPDBAdmin)
 admin.site.register(Siswa, SiswaAdmin)
 admin.site.site_header = 'Database TPA Al-Qalam'
-admin.site.site_title = 'Ahlan Wa Sahlan-Selamat Datang di Database TPA Al-Qalam'
-admin.site.index_title = 'Ahlan Wa Sahlan-Selamat Datang di Dashboard Database TPA Al-Qalam'
+admin.site.site_title = 'Ahlan Wa Sahlan di Database TPA Al-Qalam'
+admin.site.index_title = 'Ahlan Wa Sahlan di Dashboard Database TPA Al-Qalam'
 
 
 
--- mysite/database_tpa/models.py
+++ mysite/database_tpa/models.py
@@ -51,22 +51,49 @@ class SiswaPDB(models.Model):
         verbose_name_plural = 'Siswa PDB'
 
 
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 class Siswa(models.Model):
     CHOICES = (
         ('Laki-laki', 'Laki-laki'),
         ('Perempuan', 'Perempuan'),
     )
-    nama_lengkap = models.CharField(max_length=50, blank=True,null=True)
+    JILID_CHOICES = (
+        ('Pra', 'Pra'),
+        ('Jilid 1', 'Jilid 1'),
+        ('Jilid 2', 'Jilid 2'),
+        ('Jilid 3', 'Jilid 3'),
+        ('Jilid 4', 'Jilid 4'),
+        ('Jilid 5', 'Jilid 5'),
+        ('Jilid 6', 'Jilid 6'),
+        ('Al-Quran', 'Al-Quran'),
+        ('Tahfidz', 'Tahfidz'),
+        ('Private', 'Private'),
+    )
+    nama_lengkap = models.CharField(max_length=50, blank=True, null=True)
     nama_panggilan = models.CharField(max_length=20, blank=True, null=True)
-    nama_wali = models.CharField(max_length=50, blank=True,null=True)
-    kontak = models.CharField(max_length=30, blank=True,null=True)
+    nama_wali = models.CharField(max_length=50, blank=True, null=True)
+    kontak = models.CharField(max_length=30, blank=True, null=True)
     alamat = models.TextField(blank=True, null=True)
     guru = models.ForeignKey(Guru, null=True, blank=True, on_delete=models.SET_NULL)
     jadwal_mengaji = models.ForeignKey(Jadwal, blank=True, null=True, on_delete=models.SET_NULL)
+    spp = models.DecimalField(blank=True, null=True, max_digits=30, decimal_places=0)
     jenis_kelamin = models.CharField(max_length=20, blank=True, null=True, choices=CHOICES)
     keuangan = models.ForeignKey(Keuangan, blank=True, null=True, on_delete=models.SET_NULL)
-    siswa_pdb = models.ForeignKey(SiswaPDB, null=True, blank=True, on_delete=models.SET)
+    jilid_umi = models.CharField(blank=True, null=True, max_length=30, choices=JILID_CHOICES)
+    siswa_pdb = models.ForeignKey(SiswaPDB, null=True, blank=True, on_delete=models.SET_NULL)
 
+    def save(self, *args, **kwargs):
+        if self.jadwal_mengaji_id == 1 or self.jadwal_mengaji_id == 2:
+            self.spp = 150000
+        elif self.jadwal_mengaji_id == 3:
+            self.spp = 300000
+        elif self.jadwal_mengaji_id == 4:
+            self.spp = 1000000
+
+        super(Siswa, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.nama_lengkap
@@ -76,7 +103,6 @@ class Siswa(models.Model):
 
 @receiver(pre_save, sender=Siswa)
 def sync_siswa_pdb(sender, instance, **kwargs):
-    # Logika sinkronisasi
     if instance.siswa_pdb:
         instance.nama_lengkap = instance.siswa_pdb.nama_lengkap
         instance.nama_panggilan = instance.siswa_pdb.nama_panggilan
@@ -87,3 +113,4 @@ def sync_siswa_pdb(sender, instance, **kwargs):
         instance.jenis_kelamin = instance.siswa_pdb.jenis_kelamin
 
 pre_save.connect(sync_siswa_pdb, sender=Siswa)
+
